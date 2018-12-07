593b937ff25a3d147878be03da3827e84f457e4f
mariosneosecond <45677609+mariosneosecond@users.noreply.github.com>
2018-12-07 04:59:17 +0200

    Update tumblr_keys.py

--- tumblr_keys.py
+++ tumblr_keys.py
@@ -1,4 +1,4 @@
-consumer_key = ''
-consumer_secret = ''
+consumer_key = '5HxIJuaZ7hKiGPJNWy3zcbClOWZDRfEltuw0b0UMhBZX4GoUD8'
+consumer_secret = 'cnsoYrbXNJZkmFJ3b9vyFA6at3bPqD52dEwP8w87mDyGaPtHT4'
 token_key = '' 
 token_secret = ''
